d4f3288388d099617ec3e514b04bb992c5b5fae0
Max <drujbana@yandex.ru>
2026-09-07 12:09:36 +0300

    Keep API running when webhook registration times out

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ from config import settings
 from services.max_service import MaxService
 from services.telegram_service import TelegramService
 from contextlib import asynccontextmanager
+import logging
+
+logger = logging.getLogger(__name__)
 
 max_service = MaxService(settings.max_bot_token)
 telegram_service = TelegramService(settings.telegram_bot_token)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startup
-    await max_service.set_webhook(settings.max_webhook_url)
-    print("MAX webhook registered")
+    try:
+        await max_service.set_webhook(settings.max_webhook_url)
+        logger.info("MAX webhook registered")
+    except Exception:
+        logger.exception("Could not register MAX webhook during startup")
 
     if settings.telegram_webhook_url:
-        await telegram_service.set_webhook(
-            settings.telegram_webhook_url,
-            secret_token=settings.telegram_webhook_secret,
-        )
-        print("Telegram webhook registered")
+        try:
+            await telegram_service.set_webhook(
+                settings.telegram_webhook_url,
+                secret_token=settings.telegram_webhook_secret,
+            )
+            logger.info("Telegram webhook registered")
+        except Exception:
+            logger.exception("Could not register Telegram webhook during startup")
     yield
-    # Shutdown
-    print("Application shutting down")
+    logger.info("Application shutting down")
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
